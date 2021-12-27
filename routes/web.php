dba389838e652288912d02416c7e38f28506d3e6
Harveer Singh <harveersingh@virtualemployee.com>
2021-12-27 16:19:15 +0530

    change in the ur

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 */
 
 Route::get('/', function () { return redirect('/login');});
-Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
 Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
 Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
 Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
