4b8702ffdae709d5aaaad745fecdfc06a0a82475
kapil <harveersingh@virtualemployee.com>
2021-12-27 12:09:29 +0530

    ui changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,26 +14,15 @@ Login-Admin
 <div class="container">
 
 <div class="row justify-content-center">
-<div class="col-md-12 col-lg-10">
-<div class="wrap d-md-flex">
-<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
-<div class="text w-100">
-<h2>Welcome to login</h2>
-<p>Don't have an account?</p>
-<a href="#" class="btn btn-white btn-outline-white">Sign Up</a>
-</div>
-</div>
-<div class="login-wrap p-4 p-lg-5">
+<div class="col-md-12 col-lg-12">
+<div class="wrap new_center">
+<div class=" p-4 p-lg-5">
 <div class="d-flex">
 <div class="w-100">
-<h3 class="mb-4">Sign In</h3>
+<h3 class="mb-4 text-center">Login</h3>
+	<hr>
 </div>
-<div class="w-100">
-<p class="social-media d-flex justify-content-end">
-<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-</p>
-</div>
+
 </div>
 
 <form  method="POST" action="{{ route('login') }}" class="signin-form">
@@ -64,13 +53,13 @@ Login-Admin
                         @enderror
 </div>
 <div class="form-group">
-<button type="submit" class="form-control btn btn-primary submit px-3 mb-2">
+<button type="submit" class=" btn btn-info w-100 px-3 mb-2 mt-2 text-uppercase">
                             {{ __('Login') }}
                         </button>
 
                         
                         @if (Route::has('password.request'))
-                        <a class="reset_pass  text-right d-block mt-2" href="{{ route('password.request') }}">
+                        <a class="reset_pass  text-right d-block mt-2 font-14" href="{{ route('password.request') }}">
                             {{ __('Forgot Your Password?') }}
                         </a>
                         @endif
