15ae51c1b236fccd0489c05775a3840acfc00249
Harveer Singh <harveersingh@virtualemployee.com>
2021-12-27 17:30:40 +0530

    change pass functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,6 +26,17 @@ use PDF;
 
 class UserController extends Controller
 {
+
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         if (isset($request['search'])) {
@@ -408,5 +419,35 @@ class UserController extends Controller
 
 
         return view('pdf.export_pdf');
+    }
+
+    
+    public function changePasswordPost(Request $request) {
+     
+        if (!(Hash::check($request->get('current-password'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password.");
+        }
+
+        if(strcmp($request->get('current-password'), $request->get('new-password')) == 0){
+            // Current password and new password same
+            return redirect()->back()->with("error","New Password cannot be same as your current password.");
+        }
+
+        $validatedData = $request->validate([
+            'current-password' => 'required',
+            'new-password' => 'required|string|min:8|confirmed',
+        ]);
+
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('new-password'));
+        $user->save();
+
+        return redirect()->back()->with("success","Password successfully changed!");
+    }
+    public function showChangePasswordGet() {
+  
+        return view('auth.passwords.change-password');
     }
 }
