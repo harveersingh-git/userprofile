73b1cf3c68cff7637e693e85b3169e45e5270b96
Harveer Singh <harveersingh@virtualemployee.com>
2021-12-31 21:19:56 +0530

    dashboard

--- resources/views/auth/passwords/change-password.blade.php
+++ resources/views/auth/passwords/change-password.blade.php
@@ -9,6 +9,7 @@ Users
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
+                
                 <h1 class="page-header"> Change Password</h1>
             </div>
             <!-- /.col-lg-12 -->
