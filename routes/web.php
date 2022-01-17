04fe927187455b45f599316b0c10f93773e98eae
Harveer Singh <harveersingh@virtualemployee.com>
2022-01-17 21:19:56 +0530

    route isse

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::any('/logout', function () { return redirect('/home');});
+// Route::any('/logout', function () { return redirect('/login');});
 Route::any('/', function () { return redirect('/login');});
 Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
 Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
