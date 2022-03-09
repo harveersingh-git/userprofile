8defcc62478889dbbb14551352a1dc4dd9a15949
Harveer Singh <harveersingh@virtualemployee.com>
2022-03-09 12:25:03 +0530

    change the title postition

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,29 +62,7 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-6" style="height: 181px;">
-                <div class="panel panel-yellow">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-users fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">{{$data['total_users']}}</div>
-                                <div>Total Resource</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="{{url('/users')}}">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
 
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
             <div class="col-lg-2 col-md-6" style="height: 181px;">
                 <div class="panel panel-green">
                     <div class="panel-heading">
@@ -99,6 +77,29 @@
                         </div>
                     </div>
                     <a href="{{url('/clients')}}">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6" style="height: 181px;">
+                <div class="panel panel-yellow">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-users fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">{{$data['total_users']}}</div>
+                                <div>Total Resource</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="{{url('/users')}}">
                         <div class="panel-footer">
                             <span class="pull-left">View Details</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
